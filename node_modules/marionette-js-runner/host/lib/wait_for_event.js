13a6c69601dcbebbd1ccc40989bb775912eb35b5
gaye <gaye@mozilla.com>
2015-03-11 15:09:39 -0400

    marionette-js-runner 1.0.x

---
+++ node_modules/marionette-js-runner/host/lib/wait_for_event.js
@@ -0,0 +1,12 @@
+var Promise = require('promise');
+
+module.exports = function waitForEvent(listener, event) {
+  return new Promise(function(accept, reject) {
+    listener.on(event, function handler(message) {
+      // accept the message before clearing the listener
+      accept(message);
+      listener.removeListener(event, handler);
+    });
+  });
+};
+
---
+++ node_modules/marionette-js-runner/lib/require_reporter.js
@@ -0,0 +1,20 @@
+/**
+Helper to import the mocha reporters...
+*/
+module.exports = function requireReporter(argv, path) {
+  var reporter;
+  [
+    argv.reporter,
+    path,
+    'mocha/lib/reporters/' + (path || 'dot')
+  ].some(function(value) {
+    try {
+      reporter = require(value);
+      return true;
+    } catch (error) {
+      return false;
+    }
+  });
+
+  return reporter;
+};
